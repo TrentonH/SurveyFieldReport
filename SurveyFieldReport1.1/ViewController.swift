7c1b6325feddcfa25faf676c715abe8802ec3dea
Trenton Hamilton <trentonhamilton@trentons-mbp.hsd1.co.comcast.net>
2017-08-15 13:05:58 -0600

    this is the table check mark

--- SurveyFieldReport1.1/ViewController.swift
+++ SurveyFieldReport1.1/ViewController.swift
@@ -8,7 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    let fieldOption = ["House Stake, staked per plot plan", "house Stake, unable to stake",
+    "House Stake, see comments", "Foundation Form Check, passed",
+    "Foundation Form Check, FAILED", "Foundation Form Check, see comments",
+    "Rough Grade Stakes, set per plot plan", "Rough Grade Stakes, see comments",
+    "Improvements Survey, revisions needed", "Improvements Survey, flatwork not complete",
+    "Improvements Survey, see comments", "Grading Survey, passed",
+    "Grading Survey, FAILD - gradeing/flatowrk", "Grading Survey, FAILD - correction stakes set",
+    "Grading Survey, see comments"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return fieldOption.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = fieldOption[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
